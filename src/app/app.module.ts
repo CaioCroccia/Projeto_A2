ea99b49644c2f376e66e605e9c84b1f266c4ebb2
CaioCroccia <caio.carvalho15@fatec.sp.gov.br>
2022-06-17 17:29:07 -0300

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CaioHomeComponent } from './caio-home/caio-home.component';
+import { CaioTasksComponent } from './caio-tasks/caio-tasks.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(
+    {path: '', component: CaioHomeComponent},
+    {path: 'tasks', component: CaioTasksComponent},
+
+  ) ],
+  declarations: [ AppComponent, HelloComponent, CaioHomeComponent, CaioTasksComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/caio-tasks/caio-tasks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-caio-tasks',
+  templateUrl: './caio-tasks.component.html',
+  styleUrls: ['./caio-tasks.component.css']
+})
+export class CaioTasksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

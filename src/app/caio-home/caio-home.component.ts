5c6b45af34cf75055074f109dd589e63f2316303
CaioCroccia <caio.carvalho15@fatec.sp.gov.br>
2022-06-17 17:26:12 -0300

    2 files added

---
+++ src/app/caio-home/caio-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-caio-home',
+  templateUrl: './caio-home.component.html',
+  styleUrls: ['./caio-home.component.css']
+})
+export class CaioHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
